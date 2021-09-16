cf03862b5803c5b44a9ee52213e8be6bbf806a3b
Arjun Gautam <arjungautam@Arjuns-MacBook-Pro.local>
2021-09-16 23:01:54 +0545

    demo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,8 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>Home</h1>
+
+            <p>This is home</p>
             </div>
         )
     }
